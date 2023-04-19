650fcc06a28aa1a9764be05bab6e4006cb6d37d6
AndresHerrera2023 <andresfelipe.herreramotta@gmail.com>
2023-04-19 16:58:47 -0500

    Documentation

--- R/f1.R
+++ R/f1.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param text (character text)
+#'
+#' @return (A character example)
+#' @export
+#'
+#' @examples
+#' obj <- f1(text = "simple")
 f1 <- function(text = "default"){
   return(paste(text, "f1"))
 }
